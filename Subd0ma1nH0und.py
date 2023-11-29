1cb0d7115986b34e5dae501ab6e37062ab04ef26
HackerAndya <56228667+HackerAndya@users.noreply.github.com>
2023-11-29 08:07:39 +0530

    Fixed error
    - Fixed issue when we only run script without any args, stdin, inputfile
    - Added exception handling for error
    - Added error message if server gave any error

--- Subd0ma1nH0und.py
+++ Subd0ma1nH0und.py
@@ -5,18 +5,39 @@ import requests
 import argparse
 import time
 from concurrent.futures import ThreadPoolExecutor
+import json
 
 def search_query_on_crtsh(query):
     url = f'https://crt.sh/?q={query}&output=json'
     response = requests.get(url)
-    return response.json()
+
+    # Check if the response is successful (HTTP status code 200)
+    if response.status_code == 200:
+        try:
+            # Try to parse the response as JSON
+            result = response.json()
+            return result
+        except json.JSONDecodeError as e:
+            # Handle JSON decoding errors
+            print(f"Error decoding JSON for query '{query}': {e}")
+    else:
+        # Handle non-successful HTTP status codes
+        print(f"HTTP request failed for query '{query}' with status code {response.status_code}")
+
+    # Return None in case of errors
+    return None
 
 def get_common_names(result):
     return [entry.get('common_name', '') for entry in result]
 
 def process_query(query, output_file=None):
     try:
-        result = search_query_on_crtsh(query)
+        # Check if the result is empty or doesn't contain the expected data
+        if not result or not isinstance(result, list):
+            print(f"No valid data for query '{query}'")
+            print("\n" + "="*50 + "\n")
+            return
+
         common_names = get_common_names(result)
         print(f"Common names for query '{query}':")
 
@@ -29,11 +50,13 @@ def process_query(query, output_file=None):
         # Write to the output file if specified
         if output_file:
             with open(output_file, 'a') as out_file:
-                # out_file.write(f"Common names for query '{query}':\n")
                 out_file.write('\n'.join(common_names) + '\n\n')
 
     except KeyboardInterrupt:
         print("\nExecution terminated or interrupted.")
+    except Exception as e:
+        print(f"Error processing query '{query}': {e}")
+        print("\n" + "="*50 + "\n")
 
 def main():
     parser = argparse.ArgumentParser(description='Search queries on crt.sh concurrently.')
@@ -46,9 +69,12 @@ def main():
     if args.input_file_path:
         with open(args.input_file_path, 'r') as file:
             queries = file.readlines()
-    else:
+    elif not sys.stdin.isatty():
         # Read from stdin
         queries = sys.stdin.readlines()
+    else:
+        parser.print_help()
+        sys.exit()
 
     with ThreadPoolExecutor(max_workers=args.threads) as executor:
         for query in map(str.strip, queries):
